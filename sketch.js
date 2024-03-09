8d8513485dc87478e6001d52fb6355ebbf76bbc5
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-03-10 01:13:43 +0530

    adding limit 64 to the input value

--- sketch.js
+++ sketch.js
@@ -20,24 +20,29 @@ inpBtn.addEventListener('click', ()=>{
     let promp1 = prompt("Enter numbers of rows & columns (grid number): ");
     //let promp2 = prompt("Enter numbers of columnss: ");
 
-    container.textContent = "";
-    for (let i = 1; i <= parseInt(promp1); i++) {
-      const rowDiv = document.createElement("div");
-      rowDiv.classList.add("rows");
-      container.appendChild(rowDiv);
-      for (let j = 1; j <= parseInt(promp1); j++) {
-        const div = document.createElement("div");
-        div.classList.add("container-item");
-        rowDiv.appendChild(div);
-      }
+    container.textContent = "";  
+    if(parseInt(promp1)<=64) {
+        for (let i = 1; i <= parseInt(promp1); i++) {
+          const rowDiv = document.createElement("div");
+          rowDiv.classList.add("rows");
+          container.appendChild(rowDiv);
+          for (let j = 1; j <= parseInt(promp1); j++) {
+            const div = document.createElement("div");
+            div.classList.add("container-item");
+            rowDiv.appendChild(div);
+          }
+        }
     }
-    const divs = document.querySelectorAll(".container-item");
+    else{
+        container.textContent = "Grid limit 64!";
+    }
+    const divs = document.querySelectorAll(".container-item"); //creating hover effects on divs
     divs.forEach((item) => {
       item.addEventListener("mouseover", () => {
         item.style.cssText = "background: blue";
       });
     });
-    resetBtn.addEventListener("click", () => {
+    resetBtn.addEventListener("click", () => { //reset btn function
       divs.forEach((item) => {
         item.style.cssText = "background: rgb(238, 255, 90)";
       });
