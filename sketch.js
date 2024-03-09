9bd6d9dce9d6b08379ac0233f46f6b6f695630c2
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-03-09 21:39:44 +0530

    Creating a whole new project

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const btnDiv = document.createElement('div');
+document.body.appendChild(btnDiv);
+btnDiv.classList.add('btnDiv');
+
+const inpBtn = document.createElement("button"); //creating grid button
+btnDiv.appendChild(inpBtn);
+inpBtn.textContent = "Grid?";
+inpBtn.style.background = "brown";
+
+const resetBtn = document.createElement("button"); //creating reset button
+btnDiv.appendChild(resetBtn);
+resetBtn.textContent = "Reset";
+
+const container = document.createElement("div"); //creating container div
+document.body.appendChild(container);
+container.classList.add("container");
+
+
+inpBtn.addEventListener('click', ()=>{
+    let promp1 = prompt("Enter numbers of rows & columns (grid number): ");
+    //let promp2 = prompt("Enter numbers of columnss: ");
+
+    container.textContent = "";
+    for (let i = 1; i <= parseInt(promp1); i++) {
+      const rowDiv = document.createElement("div");
+      rowDiv.classList.add("rows");
+      container.appendChild(rowDiv);
+      for (let j = 1; j <= parseInt(promp1); j++) {
+        const div = document.createElement("div");
+        div.classList.add("container-item");
+        rowDiv.appendChild(div);
+      }
+    }
+    const divs = document.querySelectorAll(".container-item");
+    divs.forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        item.style.cssText = "background: blue";
+      });
+    });
+    resetBtn.addEventListener("click", () => {
+      divs.forEach((item) => {
+        item.style.cssText = "background: rgb(238, 255, 90)";
+      });
+    });
+})
+
+
+
+
